445ee339126929898c186496475bc4e62d5a5c34
Alvaro Araujo <alvarolac2@gmail.com>
2018-02-09 15:04:03 -0400

    readme folder

---
+++ AlvaroAraujo/README.mk
@@ -0,0 +1,6 @@
+#### Alvaro Araujo
+* Human Creativity Can be Facilitated Through Interacting With a Social Robot
+* Improving Human-Human Collaboration Between Children With a Social Robot
+* Analysis of Multi-party Human Interaction towards a Robot Mediator
+* Growing Growth Mindset with a Social Robot Peer
+---
